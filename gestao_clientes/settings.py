4bd6e00c0b5a4985e385f71129066b2651e56d57
EricksonLopes <ofc.erickson@gmail.com>
2020-07-03 11:15:10 -0300

    bootstrap

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['gestao-clientes-maratovadev.herokuapp.com']
+ALLOWED_HOSTS = ['gestao-clientes-maratovadev.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'clientes',
-    'home'
+    'home',
+    'bootstrapform'
 ]
 
 MIDDLEWARE = [
